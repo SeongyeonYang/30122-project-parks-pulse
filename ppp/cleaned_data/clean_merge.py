64f9ccb0e1df34c3899b74fe163d51b8fa39a79f
yhchang717 <142919607+yhchang717@users.noreply.github.com>
2024-02-24 11:05:17 -0600

    Yihuai (#1)
    * Add Climate Raw data and Water Quality data
    
    * cleaning climate data
    
    * Most part finished
    
    * Updated clean climate data
    
    * delete unused files
    
    * deleted wqp_api.py
    
    * climate data cleaned
    
    * updated climate data to time series csv
    
    * Add climate raw data
    
    * yihuai update
    
    * change file directory
    
    * Select only final climate rawdata
    
    * Merge all variables including climate, dmr, fire, light
    
    ---------
    
    Co-authored-by: 張藝懷 <yihuai@zhangyiuaideAir.cable.rcn.com>
    Co-authored-by: Yi-Huai <yhchang@uchicago.com>

---
+++ ppp/cleaned_data/clean_merge.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+# Function to clean park names and standardize year columns
+def prepare_dataframe(df, park_name_col='park_name', year_col='year'):
+    df[park_name_col] = df[park_name_col].str.strip()  # Clean park names
+    if year_col in df.columns:
+        df[year_col] = df[year_col].astype(str)  # Standardize year columns
+    return df
+
+# Load and prepare the time series data
+time_series = pd.read_csv("ppp/cleaned_data/cleaned_time_series.csv")
+time_series = prepare_dataframe(time_series, 'Park Name', 'Year')
+
+# Datasets to process
+datasets = [
+    ("acres", "ppp/cleaned_data/np-fires-annual-acres.csv"),
+    ("count", "ppp/cleaned_data/np-fires-annual-count.csv"),
+    ("dmr", "ppp/cleaned_data/np-dmr-annual.csv", ['2015', '2016', 
+                                        '2017', '2018', '2019', '2023']),
+    ("light", "ppp/cleaned_data/np-light-pollution-annual.csv")
+]
+
+# Process and merge each dataset
+for name, path, *optional in datasets:
+    df = pd.read_csv(path)
+    df = prepare_dataframe(df)
+    
+    if name == "dmr":  # Reshape the dmr DataFrame
+        df = pd.melt(df, id_vars=['park_name'], value_vars=optional[0],
+                     var_name='year', value_name='dmr')
+        pattern = r'(?:(?:National Park of ) \
+        |(?:National [a-zA-Z]+ of )|(?:))(.*?)(?: National [a-zA-Z]+|$)'
+        df['park_name'] = df['park_name'].str.extract(pattern)[0]
+        df['park_name'] = df['park_name'].replace(
+            'Sequoia and Kings Canyon', 'Sequoia')
+    df['year'] = df['year'].astype(str)
+    
+    # Rename 'park_name' to 'Park Name' for consistency
+    df.rename(columns={'park_name': 'Park Name',
+                       'year': 'Year'}, inplace=True)
+    
+    # Merge with the main time_series DataFrame
+    time_series = time_series.merge(df, how='left', on=['Year', 'Park Name'])
+
+time_series.drop(columns=['Unnamed: 0'], inplace=True)
+time_series.to_csv("ppp/cleaned_data/cleaned_time_series_all.csv")
