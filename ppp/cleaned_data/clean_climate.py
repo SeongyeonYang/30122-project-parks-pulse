a1d6614fceee8206e8f6ce1b24ac829ebcb7bbf0
yhchang717 <142919607+yhchang717@users.noreply.github.com>
2024-02-24 11:05:17 -0600

    Yihuai (#1)
    * Add Climate Raw data and Water Quality data
    
    * cleaning climate data
    
    * Most part finished
    
    * Updated clean climate data
    
    * delete unused files
    
    * deleted wqp_api.py
    
    * climate data cleaned
    
    * updated climate data to time series csv
    
    * Add climate raw data
    
    * yihuai update
    
    * change file directory
    
    * Select only final climate rawdata
    
    * Merge all variables including climate, dmr, fire, light
    
    ---------
    
    Co-authored-by: 張藝懷 <yihuai@zhangyiuaideAir.cable.rcn.com>
    Co-authored-by: Yi-Huai <yhchang@uchicago.com>

---
+++ ppp/cleaned_data/clean_climate.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+df = pd.DataFrame()
+
+def clean_climate(filename):
+    data = pd.read_csv(filename)
+    data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d', errors='coerce')
+    data['Year'] = data['datetime'].dt.year
+    # Define custom aggregation dictionary
+    agg_dict = {
+        'temp': 'mean',
+        'precip': 'sum',
+        'visibility': 'mean',
+        'uvindex': 'mean',
+        'severerisk': 'mean'
+    }
+
+    # Group by 'date' and 'park', then aggregate
+    annual_data = data.groupby(['Year', 'name']).agg(agg_dict)
+    annual_data.reset_index(inplace=True)
+    # Adjust park names
+    annual_data['name'] = annual_data['name'].str.extract('^(.*?) National Park')
+    annual_data.rename(columns={
+        'name': 'Park Name',
+        'temp': 'temp_avg',
+        'precip': 'precip_sum',
+    }, inplace=True)
+    return annual_data
+
+for i in range(1,10):
+    filename = f"ppp/raw_data/climate/climate_{i}.csv"
+    cleaned_data = clean_climate(filename)
+    df = pd.concat([df, cleaned_data], ignore_index=True)
+    
+df.sort_values(by=['Year', 'Park Name'])
+df['Park Name'].str.strip()
+output_filename = f"ppp/cleaned_data/climate/cleaned_climate.csv"
+df.to_csv(output_filename, index=False)
+print(f"Annual data saved to {output_filename}")
